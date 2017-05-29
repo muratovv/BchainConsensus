bb08a6a9d823cd8d86df9ce5b5218377f76d5e63
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-05-29 19:13:21 +0300

    Add transformation: ack -> chain

--- src/main/java/bchain/data/AckMessage.java
+++ src/main/java/bchain/data/AckMessage.java
@@ -4,4 +4,9 @@ package bchain.data;
  * ASK from chaining protocol
  */
 public abstract class AckMessage implements Transportable {
+
+    /**
+     * Retrieve existed {@link ChainMessage}
+     */
+    public abstract ChainMessage retrieveChainMessage();
 }
