b9cc2c83eab4f9dcc2417b42436a9aa2dfd7b8cf
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-05-29 23:08:34 +0300

    Add missed classes for bChain messages

--- src/main/java/bchain/data/ChainMessage.java
+++ src/main/java/bchain/data/ChainMessage.java
@@ -4,4 +4,19 @@ package bchain.data;
  * CHAIN from chaining protocol
  */
 public abstract class ChainMessage implements Transportable {
+
+    /**
+     * Generate new {@link AckMessage} for bChain cluster pipeline
+     */
+    public abstract AckMessage makeAckMessage();
+
+    /**
+     * Generate new {@link ReplyMessage} for bChain cluster pipeline
+     */
+    public abstract ReplyMessage makeReplyMessage();
+
+    public Client getClientInformation() {
+        // TODO: implement getClientInformation
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
